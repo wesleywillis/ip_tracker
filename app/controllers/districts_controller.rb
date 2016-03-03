f31a6b18d3c16c010ead75f1f603f39278a6463f
Wesley Willis <haileyw@gmail.com>
2016-03-02 20:14:22 -0800

    add websocket logic to district controller

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,4 +1,16 @@
 class DistrictsController < ApplicationController
+  options = { secure: true }
+  socket = PusherClient::Socket.new(ENV["PUSHER_APP_KEY"], options)
+  socket.subscribe('provider_sms-development')
+  @data = []
+  # Bind to a global event (can occur on either channel1 or channel2)
+  socket['provider_sms-development'].bind('channelevent') do |data|
+    nice_string = data.to_s
+    @data.push(nice_string)
+  end
+
+  socket.connect
+
   def index
     @districts = District.all
     render :layout => false
