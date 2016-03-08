37a34e5bb8c7d6a8a60df738e192be69c15a05d9
Wesley Willis <haileyw@gmail.com>
2016-03-07 19:06:33 -0800

    add start_in_range? method to shifts model

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,20 @@
 class Shift < ActiveRecord::Base
   belongs_to :carepair
+
+  def start_in_range?
+    shift_gps_string = self.start_gps.split
+    shift_gps = shift_gps_string.map! { |x| x.to_f }
+    care_pair = self.care_pair_id
+    client = CarePair.find(care_pair).client
+    client_gps = []
+    client_gps.push(client.latitude, client.longitude)
+
+    range = Geocoder::Calculations.distance_between(client_gps, shift_gps)
+
+    if range < 1
+      self.update(start_range: true)
+    elsif range > 1
+      self.update(start_range: false)
+    end
+  end
 end
