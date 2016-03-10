ad265cedad4e79e04205341e9216ec1548abe4e5
Wesley Willis <haileyw@gmail.com>
2016-03-09 19:22:16 -0800

    add skeletons for methods in care_pair and client models

--- app/models/care_pair.rb
+++ app/models/care_pair.rb
@@ -2,4 +2,9 @@ class CarePair < ActiveRecord::Base
   belongs_to :client
   belongs_to :worker
   has_many :shifts
+
+  def range_alert
+    #want admin to recieve alert when 5 shifts in a row have been F for final range
+    #want admin alerted when 20 non-consecutive shifts have been F for final range
+  end
 end
