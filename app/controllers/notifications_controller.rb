c35c0777f33adb78a891acdf7fc1381622322dd4
Wesley Willis <haileyw@gmail.com>
2016-03-14 13:57:30 -0700

    add skeleton for index streaming method in notifactions cont

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,10 @@
 class NotificationsController < ApplicationController
+  include ActionController::Live
+
+  Mime::Type.register "text/event-stream", :stream
+
+  def index
+    response.headers['Content-Type'] = 'text/event-stream'
+    # Stream content here
+  end
 end
