c93d6239bfb6ea25228cef1dd27d1423a556de29
Wesley Willis <haileyw@gmail.com>
2016-03-15 15:09:25 -0700

    refactor dormant client method into multiple models to isolate behaviors

--- app/models/care_pair.rb
+++ app/models/care_pair.rb
@@ -7,4 +7,13 @@ class CarePair < ActiveRecord::Base
     #want admin to recieve alert when 5 shifts in a row have been F for final range
     #want admin alerted when 20 non-consecutive shifts have been F for final range
   end
+
+  def self.dormant_client_ids
+    dormant_carepair_records = CarePair.find((CarePair.pluck(:id) - Shift.active_carepairs))
+    dormant_carepair_records.map{|cp| cp[:client_id]}.uniq
+  end
+
+  def self.all_client_ids
+    CarePair.pluck(:client_id).uniq
+  end
 end
