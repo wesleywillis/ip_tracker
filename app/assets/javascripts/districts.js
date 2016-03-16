f82388e0cee8aaa2333e7cb27066d3d90d81ad2b
Wesley Willis <haileyw@gmail.com>
2016-03-16 14:55:05 -0700

    add back in polling js to update alert badges

--- app/assets/javascripts/districts.js
+++ app/assets/javascripts/districts.js
@@ -1,14 +1,14 @@
 
-// setInterval(function(){
-  // $.get("/api/districts/1/shifts", function(data) {
-    // $(".js-bad-shifts").html(data.count);
-  // });
-// 
-// }, 100000000000);
-// 
-// setInterval(function(){
-  // $.get("/api/districts/1/dormant", function(data) {
-    // $(".js-dormant-cases").html(data.count);
-  // });
-// 
-// }, 1000000000000);
+setInterval(function(){
+  $.get("/api/districts/1/shifts", function(data) {
+    $(".js-bad-shifts").html(data.count);
+  });
+
+}, 1000);
+
+setInterval(function(){
+  $.get("/api/districts/1/dormant", function(data) {
+    $(".js-dormant-cases").html(data.count);
+  });
+
+}, 1000);
